31685690de3ee2c26b8e07613adf8d8e26821f67
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-07 15:51:54 +0000

    update readme fiile and fix some issues

--- cuisine-frontend/src/app/core/services/recipe/recipe.service.ts
+++ cuisine-frontend/src/app/core/services/recipe/recipe.service.ts
@@ -16,7 +16,6 @@ export class RecipeService {
     if (search) params = params.append('search', search);
     if (category) params = params.append('category', category);
 
-    console.log('📡 RecipeService.getRecipes', { search, category, url: this.apiUrl, params: params.toString() });
     return this.http.get<Recipe[]>(this.apiUrl, { params });
   }
 
@@ -27,21 +26,18 @@ export class RecipeService {
 
   // create new recipe
   createRecipe(recipe: Recipe): Observable<Recipe> {
-    console.log('📡 RecipeService.createRecipe', { url: this.apiUrl, body: recipe });
     return this.http.post<Recipe>(this.apiUrl, recipe);
   }
 
   // update existing recipee
   updateRecipe(id: number, recipe: Recipe): Observable<Recipe> {
     const url = `${this.apiUrl}/${id}`;
-    console.log('📡 RecipeService.updateRecipe', { url, body: recipe });
     return this.http.put<Recipe>(url, recipe);
   }
 
   // delete recipe
   deleteRecipe(id: number): Observable<void> {
     const url = `${this.apiUrl}/${id}`;
-    console.log('📡 RecipeService.deleteRecipe', { url });
     return this.http.delete<void>(url);
   }
 }
